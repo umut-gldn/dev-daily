3499ed95ce14b53106b05095b54bb15fb7832458
umut-gldn <umutgulden33@gmail.com>
2026-02-14 23:30:57 +0300

    Add Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
